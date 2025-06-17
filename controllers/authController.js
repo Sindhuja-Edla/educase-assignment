654eb33da9409f5be9088cbe4be3432ed96b7b74
Sindhujaedla <sindhujaedla9@gmail.com>
2025-06-17 20:51:46 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,48 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+};
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { fullName, phoneNumber, email, password, company, agency } = req.body;
+
+    const userExists = await User.findOne({ email });
+    if (userExists) return res.status(400).json({ message: 'User already exists' });
+
+    const user = await User.create({
+      fullName,
+      phoneNumber,
+      email,
+      password,
+      company,
+      agency,
+    });
+
+    res.status(201).json({ message: 'Account created successfully', token: generateToken(user._id) });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong', error });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    res.json({ 
+      message: 'You have logged in successfully!', 
+      token: generateToken(user._id),
+      name: user.fullName, // This goes into localStorage
+      email: user.email  
+    });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong', error });
+  }
+};
